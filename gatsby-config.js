5a62cfe5a7f1f5e46a749c987e7d845585d207ea
razorhollow <rob@razorhollow.com>
2023-05-02 14:43:46 -0400

    add blog skeleton

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,12 +3,19 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `razor-hollow-website`,
+    title: `Razor Hollow`,
     siteUrl: `https://www.yourdomain.tld`
   },
   plugins: [
     "gatsby-plugin-emotion",
     "gatsby-plugin-image",
-    "gatsby-plugin-sharp"
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
   ]
 };
